bbddc3a10cfef37cdb07adb8fd3f3d23145c3c17
DeAdrien Hill <deadrienhill@yahoo.com>
2022-11-29 09:27:40 -0600

    Completed Exercise 1 and 2.

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,49 @@
 {
     public class Program
     {
+        //Exercise 1
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+
+
+
+                Console.WriteLine($"When {a} is divided by {b} the results are {quotient} remainder {remainder}.");
+
+
+            //Exercise 2
+
+
+            Console.WriteLine("Please enter the radius of your circle.");
+
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+
+        }
+
+
+
+
+
+
     }
 }
